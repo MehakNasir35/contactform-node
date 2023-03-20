2d90b6fa4d35826a5f15289e33b6450e0faec5b0
Mehak Nayab <nayabmeh@corp.contoursoftware.net>
2023-03-20 17:46:25 +0500

    add fetch get byid

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
-const express = require("express");
+const express = require("express")
 const app = express()
+var cors = require('cors')
+var bodyParser = require('body-parser')
+
 const users = require("./users")
-const port = 5000;
-app.use(users);
+const port = 5000
+
+app.use(cors())
+app.use(bodyParser.json({ type: ["application/json", "application/csp-report"] }));
+app.use(users)
 
 app.listen(port, function () {
     console.log(`Example app listening on port ${port}!`)
--- users.js
+++ users.js
@@ -1,24 +1,50 @@
-const express = require('express');
+const express = require('express')
 const router = express.Router()
+var fs = require("fs");
 
 router.get('/users', function (req, res) {
-    res.send('get request');
+    fs.readFile('data.json', function (err, data) {
+
+        if (err) {
+            return console.error(err);
+        } else {
+            //send data to response
+            res.end(data)
+        }
+
+    });
 });
 
 router.post('/user', function (req, res) {
-    res.send('post request');
+    var data = fs.readFileSync('data.json');
+    //convert to json format
+    var jsonData = JSON.parse(data);
+    jsonData.push(req.body);
+    //data type must be of string type or buffer 
+    var newData = JSON.stringify(jsonData);
+    // add new data to file
+    fs.writeFile('data.json', newData, 'utf8', function (err) {
+        if (err) throw err;
+        // If no error
+        res.end(newData)
+    });
 });
 
 router.put('/user', function (req, res) {
-    res.send('update request');
+    res.send('update request')
 });
 
 router.delete('/user', function (req, res) {
-    res.send('delete request');
+    res.send('delete request')
 });
 
 router.get('/user/:id', function (req, res) {
-    res.send('get request with id');
+    var data = fs.readFileSync('data.json');
+    //convert to json format
+    var jsonData = JSON.parse(data);
+    //find user with id
+    var result = jsonData.find(jsonData => jsonData.id == req.params.id);
+    res.end(JSON.stringify(result))
 });
 
-module.exports = router;
+module.exports = router
