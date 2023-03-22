f5b07f8adb5caab7cc7ec6489cdf04a91a3a8b1c
Mehak Nayab <nayabmeh@corp.contoursoftware.net>
2023-03-22 17:33:14 +0500

    sign in | verify

--- server.js
+++ server.js
@@ -1,13 +1,55 @@
 const express = require("express")
-const app = express()
-var cors = require('cors')
-var bodyParser = require('body-parser')
-app.use(express.static('public'))
+const jwt = require("jsonwebtoken");
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const cookiesParser = require("cookie-parser");
+
 const users = require("./users")
+
+const app = express()
 const port = 5000
 
 app.use(cors())
+app.use(cookiesParser())
+
 app.use(bodyParser.json({ type: ["application/json", "application/csp-report"] }));
+
+const secretKey = "secretKey";
+
+app.get("/login", async (req, res) => {
+    const user = {
+        username: "mn",
+        password: "12345",
+        email: "mehak@gmail.com",
+    };
+
+    jwt.sign(user, secretKey, { expiresIn: "400s" }, (er, token) => {
+        if(er)
+        res.send("Cannot Login");
+        
+        res.cookie("jwt", token);
+        res.send("User Logged In");
+    });
+});
+
+const verifyToken = (req, res, next) => {
+    const token = req.cookies.jwt;
+    
+    if (!token) {
+        res.status(401).send("Invalid Token");
+    }
+
+    jwt.verify(token, secretKey, (err, authData) => {
+        if (err) {
+            res.status(401).send("Invalid Token");
+        } else {
+            req.authData = authData;
+            next();
+        }
+    });
+};
+
+app.use(verifyToken)
 app.use(users)
 
 app.listen(port, function () {
